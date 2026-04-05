b77cc074ce2eab178c0957132f9463b99607ae82
Aditya Rao <adityarao2005@gmail.com>
2026-04-05 12:03:13 -0400

    creating basic cli

--- client/src/filesystem.service.ts
+++ client/src/filesystem.service.ts
@@ -1,18 +1,21 @@
+import { ChannelCredentials } from "@grpc/grpc-js";
+import { FileSystemClient } from "./proto/FileSystemService_grpc_pb"
+import { DownloadFileRequest, FileInfoRequest, UploadFileRequestFragment } from "./proto/FileSystemService_pb";
 
+export interface IFileSystemService extends Disposable {
 
-export interface IFileSystemService {
+    uploadFile(path: string, data: AsyncGenerator<Uint8Array>): Promise<void>;
 
-    uploadFile(path: string, data: Generator<Uint8Array, void, unknown>): Promise<void>;
-
-    downloadFile(path: string): Promise<Generator<Uint8Array, void, unknown>>;
+    downloadFile(path: string): AsyncGenerator<Uint8Array>;
 
     deleteFile(path: string): Promise<void>;
 
-    getFileInfo(path: string): Promise<{ size: number; createdAt: Date; modifiedAt: Date }>;
+    getFileInfo(path: string): Promise<{ size: number; isDirectory: boolean; modifiedAt: Date }>;
 
     listFiles(directoryPath: string): Promise<string[]>;
 
     createDirectory(directoryPath: string): Promise<void>;
+
 }
 
 export type FileSystemServiceOptions = {
@@ -24,4 +27,116 @@ export type FileSystemServiceOptions = {
 
 export interface IFileSystemServiceProvider {
     getFileSystemService(options?: FileSystemServiceOptions): IFileSystemService;
+}
+
+export namespace gRPC {
+    class IFileSystemServiceClient implements IFileSystemService {
+
+        private grpcClient: FileSystemClient
+
+        constructor(options: FileSystemServiceOptions) {
+            this.grpcClient = new FileSystemClient(`${options.host}:${options.port}`, ChannelCredentials.createInsecure());
+        }
+
+        [Symbol.dispose](): void {
+            this.grpcClient.close();
+        }
+
+        uploadFile(path: string, data: AsyncGenerator<Uint8Array>): Promise<void> {
+            const promise = new Promise<void>(async (resolve, reject) => {
+                const writableStream = this.grpcClient.uploadFile((err, response) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+
+                for await (const chunk of data) {
+                    writableStream.write(new UploadFileRequestFragment().setPath(path).setData(chunk));
+                }
+
+                writableStream.end();
+            });
+
+            return promise;
+        }
+
+        async *downloadFile(path: string): AsyncGenerator<Uint8Array> {
+            const stream = this.grpcClient.downloadFile(new DownloadFileRequest().setPath(path));
+
+            for await (const response of stream) {
+                yield response.getData();
+            }
+        }
+
+        deleteFile(path: string): Promise<void> {
+            return new Promise((resolve, reject) => {
+                this.grpcClient.deleteFile(new FileInfoRequest().setPath(path), (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            });
+        }
+
+        getFileInfo(path: string): Promise<{ size: number; isDirectory: boolean; modifiedAt: Date }> {
+            return new Promise((resolve, reject) => {
+                this.grpcClient.getFileInfo(new FileInfoRequest().setPath(path), (err, response) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve({
+                            size: response.getSize(),
+                            isDirectory: response.getIsDirectory(),
+                            modifiedAt: new Date(response.getLastModifiedTime())
+                        });
+                    }
+                });
+            });
+        }
+
+        listFiles(directoryPath: string): Promise<string[]> {
+            return new Promise((resolve, reject) => {
+                const values = this.grpcClient.listFiles(new FileInfoRequest().setPath(directoryPath));
+
+                const fileNames: string[] = [];
+
+                values.on("data", (response) => {
+                    fileNames.push(response.getName());
+                });
+
+                values.on("end", () => {
+                    resolve(fileNames);
+                });
+
+                values.on("error", (err) => {
+                    reject(err);
+                });
+            });
+        }
+
+        createDirectory(directoryPath: string): Promise<void> {
+            return new Promise((resolve, reject) => {
+                this.grpcClient.createDirectory(new FileInfoRequest().setPath(directoryPath), (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            });
+        }
+
+    }
+
+    export function createGrpcFileServiceProvider(): IFileSystemServiceProvider {
+        return {
+            getFileSystemService: (opts?: FileSystemServiceOptions) => {
+                return new IFileSystemServiceClient(opts || { host: "localhost", port: 8080, path: "" });
+            }
+        }
+    }
 }
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,33 +1,84 @@
-
-import { ChannelCredentials } from "@grpc/grpc-js"
-import { GreeterClient } from "./proto/HelloService_grpc_pb"
-import { HelloRequest } from "./proto/HelloService_pb"
+import { IFileSystemService } from "./filesystem.service"
+import { Command } from "commander"
+import { color } from "console-log-colors"
+import { intro, outro, text, select, spinner, note } from '@clack/prompts';
 
 const address = process.env.GRPC_SERVER_ADDRESS || "localhost:8080"
-const name = process.env.NAME || "Aditya"
 
+const clients: IFileSystemService[] = []
 
-const credentials = ChannelCredentials.createInsecure()
-const client = new GreeterClient(address, credentials)
-const helloRequest = new HelloRequest()
-helloRequest.setName(name)
+const program = new Command()
 
-const shutdown = (exitCode: number) => {
-    client.close()
-    console.log("Client closed.")
-    setTimeout(() => process.exit(exitCode), 0)
-}
+program.version("1.0.0")
+    .description("A CLI for interacting with a remote file system service to upload and download files.")
+    .parse(process.argv)
 
-client.sayHello(helloRequest, (err, response) => {
-    if (err) {
-        console.error("Error:", err)
-        shutdown(1)
-        return
-    } else {
-        console.log("Response:", response.getMessage())
+async function homeScreen() {
+    intro(color.cyan("Welcome to the File System CLI!"))
+
+    outer:
+    while (true) {
+        note(color.yellow("This CLI allows you to interact with a remote file system service to upload and download files. Here are the connected file clients:"))
+
+        if (clients.length === 0) {
+            note(color.red("No clients connected. Please connect to a file system service to get started."))
+        } else {
+            note(color.green(clients.map((_, index) => `Client ${index + 1}`).join("\n"))) // Display connected clients
+        }
+
+        type Option = "connect" | "upload" | "download" | "list" | "delete" | "exit"
+
+        const options: { label: string; value: Option }[] = [
+            { label: "Connect to File System Service", value: "connect" },
+            { label: "Upload a File", value: "upload" },
+            { label: "Download a File", value: "download" },
+            { label: "List Files in Directory", value: "list" },
+            { label: "Delete a File", value: "delete" },
+            { label: "Exit", value: "exit" }
+        ]
+
+        const choice = await select({
+            message: color.cyan("Please select an option:"),
+            options
+        })
+
+        switch (choice) {
+            case "connect":
+                const address = await text({ message: color.yellow(`You selected: ${choice}. Enter the file system address to connect to (e.g grpc://localhost:8080):`) })
+
+                note(color.green(`Connecting to file system service at ${address.toString()}...`))
+
+                break;
+            case "upload":
+                break;
+            case "download":
+
+                break;
+            case "list":
+                break;
+            case "delete":
+                break;
+            case "exit":
+                outro(color.yellow(`You selected: ${choice}. Have a nice evening!`))
+
+                break outer
+        }
+
+        
     }
 
-    console.log("Closing client...")
-    shutdown(0)
 
-})
+    while (clients.length > 0) {
+        const client = clients.pop()
+
+        client?.[Symbol.dispose]()
+    }
+}
+
+async function main() {
+    const options = program.opts()
+
+    await homeScreen()
+}
+
+await main()
